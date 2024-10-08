25f7b8ac79b3e8073d824e39359ec932d6c5a6b6
Mikael <mikael.hatanpaa@gmail.com>
2024-10-08 17:08:54 +0200

    Created s3 bucket

--- main.tf
+++ main.tf
@@ -5,15 +5,30 @@ terraform {
         source = "hashicorp/random"
         version = "3.5.1"
     }
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
   }
 }
 
 provider "random" {
 }
 
+provider "aws" {
+
+}
+
 resource "random_string" "bucket_name" {
-    length = 16
+    length = 32
     special = false
+    lower = true
+    upper = false
+}
+
+resource "aws_s3_bucket" "example" {
+    bucket = random_string.bucket_name.result
+  
 }
 
 output "random_bucket_name_id" {
