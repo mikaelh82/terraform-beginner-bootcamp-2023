6781ac3837aea2e5eb20b5649225f11053fe3392
Mikael <mikael.hatanpaa@gmail.com>
2024-10-13 17:11:20 +0200

    Added HCP

--- main.tf
+++ main.tf
@@ -1,15 +1,11 @@
 
 terraform {
-
   cloud {
-
     organization = "my-private-test-fadsf32"
-
     workspaces {
       name = "terra-house-1"
     }
   }
-
   required_providers {
     random = {
       source  = "hashicorp/random"
@@ -21,27 +17,21 @@ terraform {
     }
   }
 }
-
 provider "random" {
 }
-
 provider "aws" {
-  region = "eu-north-1"
+  region = var.AWS_REGION
 
 }
-
 resource "random_string" "bucket_name" {
   length  = 32
   special = false
   lower   = true
   upper   = false
 }
-
 resource "aws_s3_bucket" "example" {
   bucket = random_string.bucket_name.result
-
 }
-
 output "random_bucket_name_id" {
   value = random_string.bucket_name.result
 }
---
+++ variables.tf
@@ -0,0 +1,6 @@
+variable "AWS_REGION" {
+  description = "The AWS region where resources are deployed"
+  type        = string
+  default     = "eu-north-1"
+
+}
